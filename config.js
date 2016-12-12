9925b8b1938e6fb89acca7101f355f24f32a2ef3
joelengt <joelengt@gmail.com>
2016-12-11 20:16:58 -0500

    Edit config.js connection to windows postgres local

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ var config = {
 	postgresql: {
 	  local: 'postgres://postgres:kuroyukihime2110@localhost:4002/caballeria_db', // mac joel
 	  //local: 'postgres://postgres:@localhost:5432/caballeria',         // servidor
-	  //local: 'postgres://postgres:gatogato@localhost:5432/caballeria', // windows
+	  //local: 'postgres://postgres:gatogato@localhost:5432/caballeria_db', // windows
 	  data_connect: {
 	  	user: 'joelengt', //env var: PGUSER
 	  	database: 'caballeria', //env var: PGDATABASE
--- routes/admin/dashboard/socios_clientes/filtro_dinamico/index.js
+++ routes/admin/dashboard/socios_clientes/filtro_dinamico/index.js
@@ -34,22 +34,196 @@ app.get('/table/:table_select/columns/params', isLoggedIn, function (req, res) {
        req.user.permiso === users_type.officers ||
        req.user.permiso === users_type.viewer) {
 
-       // var socio_id = Number(req.params.socio_id);
         var table_select = Number(req.params.table_select);
 
         var socio_filter_params = {
-            region: req.query.region,
-            unidad: req.query.unidad,
-            arma:   req.query.arma,
-            grado:  req.query.grado,
-            fecha:  req.query.fecha
+            tipo_socio:           req.query.tipo_socio.toLowerCase()         || 'todos',
+            situacion_socio:      req.query.situacion_socio.toLowerCase()    || 'todos',
+            tipo_pago:            req.query.tipo_pago.toLowerCase()          || 'todos',
+            situacion_trabajo:    req.query.situacion_trabajo.toLowerCase()  || 'todos',
+            carta_declaratoria:   req.query.carta_declaratoria.toLowerCase() || 'todos',
+            onomastico:           req.query.onomastico.toLowerCase()         || 'todos'
         }
 
-        console.log(socio_id);
+        var queryFiler = '';
 
         var results = [];
 
         if(table_select >= 0 && table_select <= 2) {
+
+            // Options Filter
+            if(socio_filter_params.tipo_socio ===          'todos' &&
+               socio_filter_params.situacion_socio ===     'todos' &&
+               socio_filter_params.tipo_pago ===           'todos' &&
+               socio_filter_params.situacion_trabajo ===   'todos' &&
+               socio_filter_params.carta_declaratoria ===  'todos' &&
+               socio_filter_params.onomastico ===          'todos') {
+
+               queryFiler = `SELECT * FROM ${ data_value_tablas[table_select] };`;
+
+            } else {
+                var filtro_tipo_socio = '';
+                var filtro_situacion_socio = '';
+                var filtro_tipo_pago = '';
+                var filtro_situacion_trabajo = '';
+                var filtro_carta_declaratoria = '';
+                var filtro_onomastico = '';
+
+                // Evaluando el filtro => 'Tipo Socio'
+                switch (socio_filter_params.tipo_socio) {
+                    case 'activos':
+                        filtro_tipo_socio = 'activos';
+                        break;
+
+                    case 'adherentes':
+                        filtro_tipo_socio = 'adherentes';
+                        break;
+
+                    case 'todos':
+                        // Si la palabra es 'todos', se queda en blanco
+                        filtro_tipo_socio = ''
+                        break;
+                }
+
+                // Evaluando el filtro => 'Situacion de socio'
+                switch (socio_filter_params.situacion_socio) {
+                    case 'habil':
+                        filtro_situacion_socio = 'habil';
+                        break;
+
+                    case 'inhabil':
+                        filtro_situacion_socio = 'inhabil';
+                        break;
+
+                    case 'renunciante':
+                        filtro_situacion_socio = 'renunciante';
+                        break;
+
+                    case 'fallecido':
+                        filtro_situacion_socio = 'fallecido';
+                        break;
+
+                    case 'todos':
+                        // Si la palabra es 'todos', se queda en blanco
+                        filtro_situacion_socio = '';
+                        break;
+                }
+
+                // Evaluando el filtro => 'Tipo de pago'
+                switch (socio_filter_params.tipo_pago) {
+                    case 'ogecoe':
+                        filtro_tipo_pago = 'ogecoe';
+                        break;
+
+                    case 'cpmp':
+                        filtro_tipo_pago = 'cpmp';
+                        break;
+
+                    case 'pago_directo':
+                        filtro_tipo_pago = 'pago_directo';
+                        break;
+
+                    case 'todos':
+                        filtro_tipo_pago = '';
+                        break;
+                }
+
+                // Evaluando el filtro => 'Situación de trabajo'
+                switch (socio_filter_params.situacion_trabajo) {
+                    case 'actividad':
+                        filtro_situacion_trabajo = 'actividad'
+                        break;
+
+                    case 'retiro':
+                        filtro_situacion_trabajo = 'retiro';
+                        break;
+
+                    case 'todos':
+                        filtro_situacion_trabajo = '';
+                        break;
+                }
+
+                // Evaluando el filtro => 'Carta declaratoria'
+                switch (socio_filter_params.carta_declaratoria) {
+                    case 'si':
+                        filtro_carta_declaratoria = 'si';
+                        break;
+
+                    case 'no':
+                        filtro_carta_declaratoria = 'no';
+                        break;
+
+                    case 'todos':
+                        filtro_carta_declaratoria = '';
+                        break;
+                }
+
+                // Evaluando el filtro => 'Onomastico'
+                switch (socio_filter_params.onomastico) {
+                    case 'enero':
+                        filtro_onomastico = 'enero';
+                        break;
+
+                    case 'febrero':
+                        filtro_onomastico = 'febrero';
+                        break;
+
+                    case 'marzo':
+                        filtro_onomastico = 'marzo';
+                        break;
+
+                    case 'abril':
+                        filtro_onomastico = 'abril';
+                        break;
+
+                    case 'mayo':
+                        filtro_onomastico = 'mayo';
+                        break;
+
+                    case 'junio':
+                        filtro_onomastico = 'junio';
+                        break;
+
+                    case 'julio':
+                        filtro_onomastico = 'julio';
+                        break;
+
+                    case 'agosto':
+                        filtro_onomastico = 'agosto';
+                        break;
+
+                    case 'septiembre':
+                        filtro_onomastico = 'septiembre';
+                        break;
+
+                    case 'octubre':
+                        filtro_onomastico = 'octubre';
+                        break;
+
+                    case 'noviembre':
+                        filtro_onomastico = 'noviembre';
+                        break;
+
+                    case 'diciembre':
+                        filtro_onomastico = 'diciembre';
+                        break;
+
+                    case 'todos':
+                        filtro_onomastico = '';
+                        break;
+                }
+
+                queryFiler = `SELECT * FROM ${ data_value_tablas[table_select] }
+                              WHERE  
+                              tipo_socio =         '' AND
+                              situacion_socio =    '' AND
+                              tipo_pago =          '' AND 
+                              situacion_trabajo =  '' AND 
+                              carta_declaratoria = '' AND 
+                              onomastico =         '';`
+
+            }
+
             // Get a Postgres client from the connection pool
             pg.connect(connectionString, (err, client, done) => {
                 // Handle connection errors
@@ -63,7 +237,8 @@ app.get('/table/:table_select/columns/params', isLoggedIn, function (req, res) {
                 }
                 
                 // SQL Query > Select Data
-                const query = client.query(`SELECT * FROM ${ data_value_tablas[table_select] } WHERE ${ param_filter } = '${ param_content }';`)
+                const query = client.query(`SELECT * FROM ${ data_value_tablas[table_select] } 
+                                            ${ query_filter };`)
 
                 // Stream results back one row at a time
                 query.on('row', (row) => {
